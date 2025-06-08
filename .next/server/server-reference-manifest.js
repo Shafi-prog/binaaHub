a2048c50d4d7ef8c9194325ae2e1814617962c99
shafi-prog <shs_5000@hotmail.com>
2025-06-08 15:44:17 +0300

    @refernce copy@

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HzOd/hiYJrmlctwrLie1TDHANBM5seDBwVLrMMHIdco=\"\n}"
